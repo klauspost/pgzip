fccba12d82cf377f4f4fba76e17689af4f676a4f
Klaus Post <klauspost@gmail.com>
2015-10-10 12:38:21 +0200

    Add "MultiStream" option to Reader.

--- gunzip.go
+++ gunzip.go
@@ -18,11 +18,12 @@ import (
 	"bufio"
 	"compress/flate"
 	"errors"
-	"github.com/klauspost/crc32"
 	"hash"
 	"io"
 	"sync"
 	"time"
+
+	"github.com/klauspost/crc32"
 )
 
 const (
@@ -84,6 +85,7 @@ type Reader struct {
 	buf          [512]byte
 	err          error
 	closeErr     chan error
+	multistream  bool
 
 	readAhead   chan interface{}
 	current     []byte
@@ -105,6 +107,7 @@ func NewReader(r io.Reader) (*Reader, error) {
 	z.blockSize = defaultBlockSize
 	z.r = makeReader(r)
 	z.digest = crc32.NewIEEE()
+	z.multistream = true
 	if err := z.readHeader(true); err != nil {
 		return nil, err
 	}
@@ -127,6 +130,7 @@ func NewReaderN(r io.Reader, blockSize, blocks int) (*Reader, error) {
 	z.blockSize = blockSize
 	z.r = makeReader(r)
 	z.digest = crc32.NewIEEE()
+	z.multistream = true
 	if err := z.readHeader(true); err != nil {
 		return nil, err
 	}
@@ -141,7 +145,28 @@ func (z *Reader) Reset(r io.Reader) error {
 	z.digest = crc32.NewIEEE()
 	z.size = 0
 	z.err = nil
+	z.multistream = true
 	return z.readHeader(true)
+}
+
+// Multistream controls whether the reader supports multistream files.
+//
+// If enabled (the default), the Reader expects the input to be a sequence
+// of individually gzipped data streams, each with its own header and
+// trailer, ending at EOF. The effect is that the concatenation of a sequence
+// of gzipped files is treated as equivalent to the gzip of the concatenation
+// of the sequence. This is standard behavior for gzip readers.
+//
+// Calling Multistream(false) disables this behavior; disabling the behavior
+// can be useful when reading file formats that distinguish individual gzip
+// data streams or mix gzip data streams with other data streams.
+// In this mode, when the Reader reaches the end of the data stream,
+// Read returns io.EOF. If the underlying reader implements io.ByteReader,
+// it will be left positioned just after the gzip stream.
+// To start the next stream, call z.Reset(r) followed by z.Multistream(false).
+// If there is no next stream, z.Reset(r) will return io.EOF.
+func (z *Reader) Multistream(ok bool) {
+	z.multistream = ok
 }
 
 // GZIP (RFC 1952) is little-endian, unlike ZLIB (RFC 1950).
@@ -402,7 +427,12 @@ func (z *Reader) Read(p []byte) (n int, err error) {
 		return 0, z.err
 	}
 
-	// File is ok; is there another?
+	// File is ok; should we attempt reading one more?
+	if !z.multistream {
+		return 0, io.EOF
+	}
+
+	// Is there another?
 	if err = z.readHeader(false); err != nil {
 		z.err = err
 		return
